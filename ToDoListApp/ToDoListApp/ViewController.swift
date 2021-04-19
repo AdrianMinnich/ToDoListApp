fc5b03893310e3f9b4e256bb0dd90a40b7401f36
Adrian Minnich <adrian.minnich@comarch.pl>
2021-04-19 18:57:12 +0200

    initial commit

---
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Adrian Minnich on 19/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
